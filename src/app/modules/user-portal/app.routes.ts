37af2a4e854de066f3a62051490184661d7a11cc
carlitossaul <carlitossaul.h@gmail.com>
2024-04-15 21:23:33 -0300

    Add home component and correct styles on components.

--- src/app/modules/user-portal/app.routes.ts
+++ src/app/modules/user-portal/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from '../../user-portal/components/login/login.compone
 import { RegisterComponent } from '../../user-portal/components/register/register.component';
 import { LandingPageComponent } from '../../user-portal/components/landing-page/landing-page.component';
 import { ForgotPasswordComponent } from '../../user-portal/components/forgot-password/forgot-password.component';
+import { HomeComponent } from '../../user-portal/components/home/home.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'home', component: LandingPageComponent },
+  { path: '', component: LandingPageComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'home', component: HomeComponent },
 ];
