c53bfd129f94d0780ff2227a3da4edbf88174014
Wesley Ulisses <wesleyulisses0@gmail.com>
2024-04-29 09:34:18 -0300

    Add header component for home page

--- src/app/user-portal/components/home/home.component.ts
+++ src/app/user-portal/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderHomeComponent } from './header-home/header-home.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderHomeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
