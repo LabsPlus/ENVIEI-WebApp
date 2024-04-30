bcd7ca62df431ff4643e2aae8b1ec6fb69134285
Wesley Ulisses <wesleyulisses0@gmail.com>
2024-04-30 17:29:40 -0300

    Update styles.css, home-header.interface.ts, home.component.html, user-drop-down.interface.ts, IUser.ts, home.service.spec.ts, home.service.ts, and header-home.component.html

--- src/app/user-portal/components/home/header-home/header-home.component.ts
+++ src/app/user-portal/components/home/header-home/header-home.component.ts
@@ -1,12 +1,77 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeService } from '../../../services/home/home.service';
+import IUser from '../../../interfaces/IUser';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-header-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
+  providers: [HomeService],
   templateUrl: './header-home.component.html',
   styleUrl: './header-home.component.css'
 })
 export class HeaderHomeComponent {
 
+  user: IUser = {};
+  menuOpen: boolean = false; // Estado do menu
+  acessToken: string ;
+  constructor(private homeService: HomeService) { 
+    
+    if (typeof localStorage !== 'undefined') {
+      this.acessToken = localStorage.getItem('access_token') as string;
+    } else {
+      this.acessToken = '';
+    }
+  
+    this.getUserData();
+  }
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+    
+  }
+
+  getUserData(): void {
+    
+    this.homeService.getUserData(this.acessToken).
+    toPromise().
+    then((response: HttpResponse<IUser> | any) => {
+      
+      if (response?.status == 200 || response?.status == 201) {
+        
+        this.user.name = response.body.name;
+        this.user.email = response.body.email;
+        this.user.profile_photo = response.body.profile_photo;
+        this.user.phone_number = response.body.phone_number;
+        
+      }
+
+    }).catch((error: HttpErrorResponse) => {
+      if (error.status >= 400 && error.status < 500) {
+        console.error(error.error.error);
+      }
+
+      if (error.status >= 500) {
+        console.error('Erro interno no servidor.');
+      }
+
+      this.user.name = 'Default';
+      this.user.email = '';
+      this.user.profile_photo = '../../../../../assets/images/shared/not-registred-user-photo.png';
+      this.user.phone_number = '';
+
+    });
+  }
+
+
+  goToProfile(): void {
+    console.log('Navigating to profile...');
+  }
+
+  logout(): void {
+    localStorage.removeItem('access_token');  
+  }
+
+  
 }
