4e81b2782ea689e3452e31e21ca08376bed8968e
Wesley Ulisses <wesleyulisses0@gmail.com>
2024-05-23 15:03:52 -0300

    chore: Add close button to change personal information modal component

--- src/app/user-portal/components/change-personal-information-modal/change-personal-information-modal.component.ts
+++ src/app/user-portal/components/change-personal-information-modal/change-personal-information-modal.component.ts
@@ -1,17 +1,21 @@
-import {Component} from '@angular/core';
-import {MatDialog, MatDialogModule} from '@angular/material/dialog';
-import {MatButtonModule} from '@angular/material/button';
+import { Component } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { UserService } from '../../services/user-service/user.service';
+import  IUser from '../../interfaces/IUser';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-change-personal-information-modal',
   standalone: true,
   imports: [MatButtonModule, MatDialogModule],
+  providers: [UserService],
   templateUrl: './change-personal-information-modal.component.html',
   styleUrl: './change-personal-information-modal.component.css'
 })
 export class ChangePersonalInformationModalComponent {
 
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog, private userService: UserService) { }
 
   openDialog() {
     const dialogRef = this.dialog.open(ChangePersonalInformationModalComponent);
@@ -20,4 +24,26 @@ export class ChangePersonalInformationModalComponent {
     });
   }
 
+
+  userProfile: IUser = {};
+  acessToken: string = '';
+
+  updateProfileData(): void {
+    this.userService
+      .updateUser( this.userProfile, this.acessToken,)
+      .toPromise()
+      .then((response: HttpResponse<IUser> | any) => {
+        if (response?.status == 200 || response?.status == 201) {
+          this.userProfile.name = response.body.name;
+          this.userProfile.email = response.body.email;
+          this.userProfile.profile_photo = response.body.profile_photo;
+          this.userProfile.email_recovery = response.body.email_recovery;
+          this.userProfile.cpf_cnpj = response.body.cpf_cnpj;
+          this.userProfile.phone_number = response.body.phone_number;
+        }
+      })
+      .catch((error: HttpErrorResponse) => {
+        console.error(error);
+      });
+  }
 }
