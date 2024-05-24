1f0eb92f1e3d899d04b99fed526953933787b864
Wesley Ulisses <wesleyulisses0@gmail.com>
2024-05-24 17:12:48 -0300

    chore: Update personal information modal component with phone number and password validator services

--- src/app/user-portal/components/change-personal-information-modal/change-personal-information-modal.component.ts
+++ src/app/user-portal/components/change-personal-information-modal/change-personal-information-modal.component.ts
@@ -1,35 +1,40 @@
 import { Component } from '@angular/core';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
-import { UserService } from '../../services/user-service/user.service';
-import IUser from '../../interfaces/IUser';
-import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
-import { ToastrNotificationService } from '../../services/toastr/toastr.service';
-import { PasswordValidatorService } from '../../../shared/services/password-validator/password-validator.service';
-import { NameValidatorService } from '../../../shared/services/name-validator/name-validator.service';
-import { PhoneNumberValidatorService } from '../../../shared/services/phone-number-validator/phone-number-validator.service';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import IUser from '../../interfaces/IUser';
+import { UserService } from '../../services/user-service/user.service';
+import { ToastrNotificationService } from '../../services/toastr/toastr.service';
+import { NameValidatorService } from '../../../shared/services/name-validator/name-validator.service';
+import { PasswordValidatorService } from '../../../shared/services/password-validator/password-validator.service';
+import { PhoneNumberValidatorService } from '../../../shared/services/phone-number-validator/phone-number-validator.service';
 
 @Component({
   selector: 'app-change-personal-information-modal',
   standalone: true,
   imports: [MatButtonModule, MatDialogModule, ReactiveFormsModule],
-  providers: [UserService, ToastrNotificationService, PasswordValidatorService, NameValidatorService, PhoneNumberValidatorService ],
+  providers: [UserService, ToastrNotificationService, PasswordValidatorService, NameValidatorService, PhoneNumberValidatorService],
   templateUrl: './change-personal-information-modal.component.html',
   styleUrl: './change-personal-information-modal.component.css'
 })
+
 export class ChangePersonalInformationModalComponent {
 
-  userForm! : FormGroup;
+  acessToken: string = '';
+  userForm!: FormGroup;
+  userProfile: IUser = {};
+  password: string = '';
 
   constructor
-  (
-    public dialog: MatDialog, private userService: UserService, 
-    private toarstNotification: ToastrNotificationService, 
-    private passwordValidator: PasswordValidatorService, 
-    private nameValidator: NameValidatorService,
-    private phoneNumberValidator: PhoneNumberValidatorService
-  ) { 
+    (
+      public dialog: MatDialog, private userService: UserService,
+      private toarstNotification: ToastrNotificationService,
+      private passwordValidator: PasswordValidatorService,
+      private nameValidator: NameValidatorService,
+      private phoneNumberValidator: PhoneNumberValidatorService
+    ) {
+
     this.getToken();
 
     this.userForm = new FormGroup({
@@ -40,26 +45,6 @@ export class ChangePersonalInformationModalComponent {
 
   }
 
-  openDialog() {
-    const dialogRef = this.dialog.open(ChangePersonalInformationModalComponent);
-
-    dialogRef.afterClosed().subscribe(result => {
-    });
-  }
-
-
-  userProfile: IUser = {};
-  acessToken: string = '';
-
-
-  getFormData(): void {
-
-    this.userProfile.name = this.userForm.get('name')?.value;
-    this.userProfile.phone_number = this.userForm.get('phone_number')?.value;
-    this.userProfile.password = this.userForm.get('password')?.value;
-  
-  }
-  
   updateProfileData(userProfileData: IUser): void {
 
     this.userService
@@ -80,9 +65,9 @@ export class ChangePersonalInformationModalComponent {
       });
   }
 
-  validateUserPassword(password: string): Promise<boolean> {
+  async validateUserPassword(password: string): Promise<boolean> {
 
-    return this.userService
+    return await this.userService
       .validateUserPassword(password, this.acessToken)
       .toPromise()
       .then((response: HttpResponse<any> | undefined) => {
@@ -92,7 +77,6 @@ export class ChangePersonalInformationModalComponent {
         }
 
         if (response?.body.isValidPassword == false) {
-          this.toarstNotification.showError('Senha inválida', 'Erro');
           return false;
         }
 
@@ -109,22 +93,15 @@ export class ChangePersonalInformationModalComponent {
 
   }
 
-  getToken(): void {
+  async submit(): Promise<void> {
 
-    if (sessionStorage.getItem('accessToken') == null) {
-      this.acessToken = '';
+    this.getFormData();
+
+    if (!this.isFormValid()) {
+      this.toarstNotification.showError('Preencha todos os campos', 'Erro');
       return;
     }
 
-    this.acessToken = sessionStorage.getItem('accessToken') as string;
-
-  }
-
-
-  async submit(): Promise<void> {
-    
-    this.getFormData();
-    
     if (!this.nameValidator.validateName(this.userProfile.name as string)) {
       this.toarstNotification.showError('Nome inválido', 'Erro');
       return;
@@ -135,12 +112,12 @@ export class ChangePersonalInformationModalComponent {
       return;
     }
 
-    if (!this.passwordValidator.isPasswordFormatValid(this.userProfile.password as string)) {
+    if (!this.passwordValidator.isPasswordFormatValid(this.password)) {
       this.toarstNotification.showError('Senha inválida', 'Erro');
       return;
     }
 
-    if (!await this.validateUserPassword(this.userProfile.password as string)) {
+    if (!await this.validateUserPassword(this.password)) {
       this.toarstNotification.showError('Senha inválida', 'Erro');
       return;
     }
@@ -149,12 +126,47 @@ export class ChangePersonalInformationModalComponent {
 
     this.dialog.closeAll();
 
-    this.refresh();
+    this.refreshPage();
 
   }
 
-  refresh(): void {
+  refreshPage(): void {
     window.location.reload();
+  }
+
+  openDialog() {
+
+    const dialogRef = this.dialog.open(ChangePersonalInformationModalComponent);
+    dialogRef.afterClosed().subscribe(result => { });
+
+  }
+
+  isFormValid(): boolean {
+      
+      if (this.userForm.get('name')?.value == '' || this.userForm.get('phone_number')?.value == '' || this.userForm.get('password')?.value == '') {
+        return false;
+      }
+  
+      return true;
+  }
+
+  getFormData(): void {
+
+    this.userProfile.name = this.userForm.get('name')?.value;
+    this.userProfile.phone_number = this.userForm.get('phone_number')?.value;
+    this.password = this.userForm.get('password')?.value;
+
+  }
+
+  getToken(): void {
+
+    if (sessionStorage.getItem('accessToken') == null) {
+      this.acessToken = '';
+      return;
+    }
+
+    this.acessToken = sessionStorage.getItem('accessToken') as string;
+
   }
 
 }
