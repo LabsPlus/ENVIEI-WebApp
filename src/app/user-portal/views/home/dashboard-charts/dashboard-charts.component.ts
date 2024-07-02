8d49361f60bde5632753ff21096a2121ecea671a
carlitossaul <carlitossaul.h@gmail.com>
2024-07-02 15:50:38 -0300

    aligning divs and positioning elements

--- src/app/user-portal/views/home/dashboard-charts/dashboard-charts.component.ts
+++ src/app/user-portal/views/home/dashboard-charts/dashboard-charts.component.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy,ViewEncapsulation } from '@angular/core';
 import { StackedBarChartComponent } from '../../../../shared/components/charts/stacked-bar-chart/stacked-bar-chart.component';
 import { LineShartComponent } from '../../../../shared/components/charts/line-shart/line-shart.component';
 import { IndicatorChartComponent } from '../../../../shared/components/charts/indicator-chart/indicator-chart.component';
+import { Subscription } from 'rxjs';
+import { SidebarService } from '../../../services/sidebar/sidebar.service';
+import { CommonModule } from '@angular/common';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-dashboard-charts',
   standalone: true,
-  imports: [StackedBarChartComponent, LineShartComponent, IndicatorChartComponent],
+  imports: [MatFormFieldModule, MatSelectModule, FormsModule, ReactiveFormsModule,StackedBarChartComponent, LineShartComponent, IndicatorChartComponent, CommonModule],
   templateUrl: './dashboard-charts.component.html',
   styleUrl: './dashboard-charts.component.css'
 })
-export class DashboardChartsComponent {
+export class DashboardChartsComponent implements OnDestroy{
+  panelColor = new FormControl('red');
+  isNavOpen = false;
+  sidebarOpenSubscription: Subscription;
 
+  constructor(private sidebarService: SidebarService) {
+    this.sidebarOpenSubscription = this.sidebarService.sidebarOpen$.subscribe(
+      (isOpen) => {
+        this.isNavOpen = isOpen;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.sidebarOpenSubscription.unsubscribe();
+  }
 }
